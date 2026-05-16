b504349203099bbbb74676d6cc4e5d346482a07e
Afaandii <fandisantuy140@gmail.com>
2026-05-16 21:21:24 +0700

    test init method query func crud

--- backend/app/Models/M_User.php
+++ backend/app/Models/M_User.php
@@ -70,4 +70,22 @@ class M_user extends Model
         $this->join('role', 'role.id = user.role_id');
         return $this->findAll();
     }
+
+    public function getUserById(int $id){
+        $this->select("user as u.*", "role.nama_role");
+        $this->join("role as r", "r.id = u.role_id");
+        return $this->where("u.id", $id)->first();
+    }
+
+    public function insertUser(array $data){
+        $this->insert($data);
+    }
+
+    public function updateUser(int $id, array $data){
+        $this->update($id, $data);
+    }
+
+    public function deleteUser(int $id){
+        return $this->delete($id);
+    }
 }
