1180a78f8f511fb27c9c87afcf92d7629bc2c975
Afaandii <fandisantuy140@gmail.com>
2026-05-22 20:56:24 +0700

    test api crud role and reupdate env file and fix error routing

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -1,19 +1,28 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
-use App\Controllers\Role;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
 
-$routes->group('api', function($routes){
+$routes->group('api', function($routes) {
   $routes->group('v1', function($routes){
-    $routes->get('role', function(){
-      return "tetstt!";
-    });
-    $routes->get('role', 'Role::index');
-    $routes->post('role', [Role::class, 'store']);
+
+    // Roles routing
+    $routes->get('role', 'role\Role::index');
+    $routes->get('role/(:num)', 'role\Role::show/$1');
+    $routes->post('role', 'role\Role::store');
+    $routes->put('role/(:num)', 'role\Role::update/$1');
+    $routes->delete('role/(:num)', 'role\Role::delete/$1');
+
+    // User Routing
+    $routes->get('user', 'user\User::index');
+    $routes->get('user/(:num)', 'user\User::show/$1');
+    $routes->post('user', 'user\User::store');
+    $routes->put('user/(:num)', 'user\User::update/$1');
+    $routes->delete('user/(:num)', 'user\User::delete/$1');
   });
 });
+?>
