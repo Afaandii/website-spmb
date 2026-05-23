ee0e4e5377ef27ec30645f104eeef3bb71faac80
Afaandii <fandisantuy140@gmail.com>
2026-05-23 10:59:30 +0700

    crud pendaftaran, jalur daftar, dokumen, data ortu and routing segment API

--- backend/app/Controllers/jalur_daftar/jalurDaftar.php
+++ backend/app/Controllers/jalur_daftar/jalurDaftar.php
@@ -3,11 +3,126 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\M_jalur_daftar;
+use CodeIgniter\API\ResponseTrait;
 
 class JalurDaftar extends BaseController
 {
+    use ResponseTrait;
+
+    /**
+     * @var M_jalur_daftar
+     */
+
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new M_jalur_daftar();
+    }
+
     public function index()
     {
-        //
+        $data = $this->model->findAll();
+        
+        return $this->respond([
+            'data' => $data,
+            'status' => 200,
+            'message' => 'Berhasil mengambil data jalur daftar!'
+        ]);
+    }
+
+    public function show(int $id){
+        if(!$this->model->find($id)){
+            return $this->respond([
+                'data' => null,
+                'status' => 403,
+                'message' => 'Id jalur daftar tidak ditemukan!'
+            ]);
+        }
+        
+        $data = $this->model->find($id);
+        
+        if($data){
+            return $this->respond([
+                'data' => $data,
+                'status' => 200,
+                'message' => 'Berhasil mengambil data jalur daftar!'
+            ]);
+        } else {
+            return $this->respond([
+                'data' => null,
+                'status' => 403,
+                'message' => 'Data jalur daftar tidak ditemukan!'
+            ]);
+        }
+    }
+
+    public function store(){
+        $data = $this->request->getPost();
+
+        if($this->model->insert($data)){
+            return $this->respond([
+                'data' => null,
+                'status' => 200,
+                'message' => 'Berhasil menambahkan data jalur daftar!'
+            ]);
+        } else {
+            return $this->respond([
+                'data' => null,
+                'status' => 403,
+                'message' => 'Gagal menambahkan data jalur daftar!'
+            ]);
+        }
+    }
+
+    public function edit(int $id){
+        if(!$this->model->find($id)){
+            return $this->respond([
+                'data' => null,
+                'status' => 403,
+                'message' => 'Id jalur daftar tidak ditemukan!'
+            ]);
+        }
+
+        $data = $this->request->getPost();
+
+        if($this->model->update($id, $data)){
+            return $this->respond([
+                'data' => null,
+                'status' => 200,
+                'message' => 'Berhasil mengubah data jalur daftar!'
+            ]);
+        } else {
+            return $this->respond([
+                'data' => null,
+                'status' => 403,
+                'message' => 'Gagal mengubah data jalur daftar!'
+            ]);
+        }
+    }
+
+    public function delete(int $id){
+        if(!$this->model->find($id)){
+            return $this->respond([
+                'data' => null,
+                'status' => 403,
+                'message' => 'Id jalur daftar tidak ditemukan!'
+            ]);
+        }
+
+        if($this->model->delete($id)){
+            return $this->respond([
+                'data' => null,
+                'status' => 200,
+                'message' => 'Berhasil menghapus data jalur daftar!'
+            ]);
+        } else {
+            return $this->respond([
+                'data' => null,
+                'status' => 403,
+                'message' => 'Gagal menghapus data jalur daftar!'
+            ]);
+        }
     }
 }
