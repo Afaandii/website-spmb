628a8f23d95cd96ddeb0297beaa6bbade8061de2
Afaandii <fandisantuy140@gmail.com>
2026-05-24 20:36:05 +0700

    rechange and check method query controller handling crud all

--- backend/app/Controllers/jalur_daftar/jalurDaftar.php
+++ backend/app/Controllers/jalur_daftar/jalurDaftar.php
@@ -23,106 +23,154 @@ class JalurDaftar extends BaseController
 
     public function index()
     {
-        $data = $this->model->findAll();
+        $data = $this->model->db->query("SELECT * FROM jalur_daftar")->getResult();
         
         return $this->respond([
             'data' => $data,
             'status' => 200,
             'message' => 'Berhasil mengambil data jalur daftar!'
-        ]);
+        ], 200);
     }
 
     public function show(int $id){
         if(!$this->model->find($id)){
             return $this->respond([
-                'data' => null,
+                'error' => $this->model->errors() ? $this->model->errors() : 'Id jalur daftar tidak ditemukan!',
                 'status' => 403,
                 'message' => 'Id jalur daftar tidak ditemukan!'
-            ]);
+            ], 403);
         }
         
-        $data = $this->model->find($id);
+        $data = $this->model->db->query("SELECT * FROM jalur_daftar WHERE id = ?", [$id])->getRow();
         
         if($data){
             return $this->respond([
                 'data' => $data,
                 'status' => 200,
                 'message' => 'Berhasil mengambil data jalur daftar!'
-            ]);
+            ], 200);
         } else {
             return $this->respond([
                 'data' => null,
                 'status' => 403,
                 'message' => 'Data jalur daftar tidak ditemukan!'
-            ]);
+            ], 403);
         }
     }
 
     public function store(){
-        $data = $this->request->getPost();
+        $request = $this->request->getVar();
 
-        if($this->model->insert($data)){
+        $dataStore = [
+            'nama_jalur' => $request['nama_jalur'],
+            'tipe_seleksi' => $request['tipe_seleksi'],
+            'kuota' => $request['kuota'],
+            'metode_perankingan' => $request['metode_perankingan'],
+            'tanggal_mulai' => $request['tanggal_mulai'],
+            'tanggal_selesai' => $request['tanggal_selesai'],
+            'is_active' => $request['is_active'],
+            'created_at' => date('Y-m-d H:i:s'),
+        ];
+
+        if(!$this->model->insert($dataStore)){
             return $this->respond([
-                'data' => null,
-                'status' => 200,
-                'message' => 'Berhasil menambahkan data jalur daftar!'
-            ]);
-        } else {
-            return $this->respond([
-                'data' => null,
+                'error' => $this->model->errors() ? $this->model->errors() : 'Gagal menyimpan data jalur daftar!',
                 'status' => 403,
-                'message' => 'Gagal menambahkan data jalur daftar!'
-            ]);
+                'message' => 'Gagal menyimpan data jalur daftar!'
+            ], 403);
         }
+
+        return $this->respondCreated([
+            'data' => null,
+            'status' => 201,
+            'message' => 'Berhasil menyimpan data jalur daftar!'
+        ], 201);
     }
 
     public function edit(int $id){
         if(!$this->model->find($id)){
             return $this->respond([
-                'data' => null,
+                'error' => $this->model->errors() ? $this->model->errors() : 'Id jalur daftar tidak ditemukan!',
                 'status' => 403,
                 'message' => 'Id jalur daftar tidak ditemukan!'
-            ]);
+            ], 403);
         }
-
-        $data = $this->request->getPost();
-
-        if($this->model->update($id, $data)){
+        
+        $data = $this->model->db->query("SELECT * FROM jalur_daftar WHERE id = ?", [$id])->getRow();
+        
+        if($data){
             return $this->respond([
-                'data' => null,
+                'data' => $data,
                 'status' => 200,
-                'message' => 'Berhasil mengubah data jalur daftar!'
-            ]);
+                'message' => 'Berhasil mengambil data jalur daftar!'
+            ], 200);
         } else {
             return $this->respond([
                 'data' => null,
                 'status' => 403,
-                'message' => 'Gagal mengubah data jalur daftar!'
-            ]);
+                'message' => 'Data jalur daftar tidak ditemukan!'
+            ], 403);
         }
+    }
+
+    public function update(int $id){
+        if(!$this->model->find($id)){
+            return $this->respond([
+                'error' => $this->model->errors() ? $this->model->errors() : 'Id jalur daftar tidak ditemukan!',
+                'status' => 403,
+                'message' => 'Id jalur daftar tidak ditemukan!'
+            ], 403);
+        }
+
+        $request = $this->request->getRawInput();
+
+        $dataUpdate = [
+            'nama_jalur' => $request['nama_jalur'],
+            'tipe_seleksi' => $request['tipe_seleksi'],
+            'kuota' => $request['kuota'],
+            'metode_perankingan' => $request['metode_perankingan'],
+            'tanggal_mulai' => $request['tanggal_mulai'],
+            'tanggal_selesai' => $request['tanggal_selesai'],
+            'is_active' => $request['is_active'],
+            'updated_at' => date('Y-m-d H:i:s'),
+        ];
+
+        if(!$this->model->update($id, $dataUpdate)){
+            return $this->respond([
+                'error' => $this->model->errors() ? $this->model->errors() : 'Gagal mengupdate data jalur daftar!',
+                'status' => 403,
+                'message' => 'Gagal mengupdate data jalur daftar!'
+            ], 403);
+        }
+
+        return $this->respondUpdated([
+            'data' => null,
+            'status' => 200,
+            'message' => 'Berhasil mengupdate data jalur daftar!'
+        ], 200);
     }
 
     public function delete(int $id){
         if(!$this->model->find($id)){
             return $this->respond([
-                'data' => null,
+                'error' => $this->model->errors() ? $this->model->errors() : 'Id jalur daftar tidak ditemukan!',
                 'status' => 403,
                 'message' => 'Id jalur daftar tidak ditemukan!'
-            ]);
+            ], 403);
         }
 
-        if($this->model->delete($id)){
+        if(!$this->model->delete($id)){
             return $this->respond([
-                'data' => null,
-                'status' => 200,
-                'message' => 'Berhasil menghapus data jalur daftar!'
-            ]);
-        } else {
-            return $this->respond([
-                'data' => null,
+                'error' => $this->model->errors() ? $this->model->errors() : 'Gagal menghapus data jalur daftar!',
                 'status' => 403,
                 'message' => 'Gagal menghapus data jalur daftar!'
-            ]);
+            ], 403);
         }
+
+        return $this->respondDeleted([
+            'data' => null,
+            'status' => 200,
+            'message' => 'Berhasil menghapus data jalur daftar!'
+        ], 200);
     }
 }
--- backend/app/Controllers/pengumuman/pengumuman.php
+++ backend/app/Controllers/pengumuman/pengumuman.php
@@ -22,88 +22,160 @@ class Pengumuman extends BaseController
 
     public function index()
     {
-        $data = $this->model->findAll();
+        $data = $this->model->db->query("SELECT * FROM pengumuman")->getResult();
 
         return $this->respond([
             'data' => $data,
             'status' => 200,
             'message' => 'Berhasil mengambil data pengumuman!'
-        ]);
+        ], 200);
     }
 
     public function show(int $id){
         if(!$this->model->find($id)){
             return $this->respond([
-                'data' => null,
+                'error' => $this->model->errors() ? $this->model->errors() : 'Id pengumuman tidak ditemukan!',
                 'status' => 403,
                 'message' => 'Id pengumuman tidak ditemukan!'
-            ]);
+            ], 403);
         }
         
-        $data = $this->model->find($id);
+        $data = $this->model->db->query("SELECT * FROM pengumuman WHERE id = ?", [$id])->getRow();
         
         if($data){
             return $this->respond([
                 'data' => $data,
                 'status' => 200,
                 'message' => 'Berhasil mengambil data pengumuman!'
-            ]);
+            ], 200);
         } else {
             return $this->respond([
                 'data' => null,
                 'status' => 403,
                 'message' => 'Data pengumuman tidak ditemukan!'
-            ]);
+            ], 403);
         }
     }
 
     public function store(){
-        $data = $this->request->getPost();
+        $request = $this->request->getVar();
 
-        $this->model->insert($data);
+        $dataStore = [
+            'jalur_id' => $request['jalur_id'],
+            'dibuat_oleh' => $request['dibuat_oleh'],
+            'tahun_ajaran_id' => $request['tahun_ajaran_id'],
+            'judul' => $request['judul'],
+            'slug' => $request['slug'],
+            'content' => $request['content'],
+            'kategori' => $request['kategori'],
+            'thumbnail' => $request['thumbnail'],
+            'tanggal_publish' => $request['tanggal_publish'],
+            'status_publish' => $request['status_publish'],
+            'created_at' => date('Y-m-d H:i:s'),
+        ];
+
+        if(!$this->model->insert($dataStore)){
+            return $this->respond([
+                'error' => $this->model->errors() ? $this->model->errors() : 'Gagal menambahkan pengumuman!',
+                'status' => 403,
+                'message' => 'Gagal menambahkan pengumuman!'
+            ], 403);
+        }
 
         return $this->respond([
             'data' => null,
             'status' => 201,
             'message' => 'Pengumuman berhasil ditambahkan!'
-        ]);
+        ], 201);
     }
 
     public function edit(int $id){
         if(!$this->model->find($id)){
             return $this->respond([
-                'data' => null,
+                'error' => $this->model->errors() ? $this->model->errors() : 'Id pengumuman tidak ditemukan!',
                 'status' => 403,
                 'message' => 'Id pengumuman tidak ditemukan!'
-            ]);
+            ], 403);
         }
 
-        $data = $this->request->getPost();
+        $data = $this->model->db->query("SELECT * FROM pengumuman WHERE id = ?", [$id])->getRow();
 
-        $this->model->update($id, $data);
+        if($data){
+            return $this->respond([
+                'data' => $data,
+                'status' => 200,
+                'message' => 'Berhasil mengambil data pengumuman!'
+            ], 200);
+        } else {
+            return $this->respond([
+                'data' => null,
+                'status' => 403,
+                'message' => 'Data pengumuman tidak ditemukan!'
+            ], 403);
+        }
+    }
+
+    public function update(int $id){
+        if(!$this->model->find($id)){
+            return $this->respond([
+                'error' => $this->model->errors() ? $this->model->errors() : 'Id pengumuman tidak ditemukan!',
+                'status' => 403,
+                'message' => 'Id pengumuman tidak ditemukan!'
+            ], 403);
+        }
+
+        $request = $this->request->getRawInput();
+
+        $dataUpdate = [
+            'jalur_id' => $request['jalur_id'],
+            'dibuat_oleh' => $request['dibuat_oleh'],
+            'tahun_ajaran_id' => $request['tahun_ajaran_id'],
+            'judul' => $request['judul'],
+            'slug' => $request['slug'],
+            'content' => $request['content'],
+            'kategori' => $request['kategori'],
+            'thumbnail' => $request['thumbnail'],
+            'tanggal_publish' => $request['tanggal_publish'],
+            'status_publish' => $request['status_publish'],
+            'updated_at' => date('Y-m-d H:i:s'),
+        ];
+
+        if(!$this->model->update($id, $dataUpdate)){
+            return $this->respond([
+                'error' => $this->model->errors() ? $this->model->errors() : 'Gagal mengubah pengumuman!',
+                'status' => 403,
+                'message' => 'Gagal mengubah pengumuman!'
+            ], 403);
+        }
 
         return $this->respond([
             'data' => null,
             'status' => 200,
             'message' => 'Pengumuman berhasil diubah!'
-        ]);
+        ], 200);
     }
 
     public function delete(int $id){
         if(!$this->model->find($id)){
             return $this->respond([
-                'data' => null,
+                'error' => $this->model->errors() ? $this->model->errors() : 'Id pengumuman tidak ditemukan!',
                 'status' => 403,
                 'message' => 'Id pengumuman tidak ditemukan!'
-            ]);
+            ], 403);
         }
 
-        $this->model->delete($id);
+        if(!$this->model->delete($id)){
+            return $this->respond([
+                'error' => $this->model->errors() ? $this->model->errors() : 'Gagal menghapus pengumuman!',
+                'status' => 403,
+                'message' => 'Gagal menghapus pengumuman!'
+            ], 403);
+        }
 
         return $this->respond([
             'data' => null,
             'status' => 200,
             'message' => 'Pengumuman berhasil dihapus!'
-        ]);
+        ], 200);
     }
 }
